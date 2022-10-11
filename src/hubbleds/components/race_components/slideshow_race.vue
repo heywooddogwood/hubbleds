415bf7ee699128674894e0c9da06f153f5533f68
Harry Houghton <henry.houghton@cfa.harvard.edu>
2022-10-10 23:57:20 -0400

    add place for race image to dialog
    needed to convert to column structure, and added a placeholder for the running race image.

--- src/hubbleds/components/race_components/slideshow_race.vue
+++ src/hubbleds/components/race_components/slideshow_race.vue
@@ -2,7 +2,7 @@
   <v-dialog
       v-model="state.race_dialog"
       persistent
-      max-width="800px"
+      max-width="1000px"
   >
     <v-card
       class="mx-auto race_dialog"
@@ -20,38 +20,60 @@
         <div
           class="mb-4"
         >
-          <p>
-            To understand Hubble’s thinking about this, let’s use the following situation as a model: Imagine runners in the middle of a race, where the runners are running at different speeds.
-          </p>
-          <p>
-            In this diagram, runner A is running with a velocity of 4 km per hour and has run a distance of 12 km, while runner C is running faster with a velocity of 10 km per hour and has gone a distance of 30 km.
-          </p>
-          <p>
-            Can you figure out how long ago the race started — the “age” of the race? (Assume each runner has maintained a consistent speed for the entire race).
-          </p>
           <v-container
             class="px-0"
             fluid
           >
-            <p>The race began:</p>
-            <mc-radiogroup
-              :radio-options="[
-                '1/3 of an hour ago.',
-                '3 hours ago.',
-                '12 hours ago.',
-                '75 hours ago.'
-              ]"
-              :feedbacks="[
-                'Recorded. You think the race began 1/3 of an hour ago.',
-                'Recorded. You think the race began 3 hours ago.',
-                'Recorded. You think the race began 12 hours ago.',
-                'Recorded. You think the race began 75 hours ago.'
-              ]"
-              :correct-answers="[1]"
-              :neutral-answers='[0,2,3]'
-              :selected-callback="(state) => { $emit('ready'); }"
-            >
-            </mc-radiogroup>
+            <v-row>
+              <v-col
+                cols="8"
+              >
+                <p>
+                  To understand Hubble’s thinking about this, let’s use the following situation as a model: Imagine runners in the middle of a race, where the runners are running at different speeds.
+                </p>
+                <p>
+                  In this diagram, runner A is running with a velocity of 4 km per hour and has run a distance of 12 km, while runner C is running faster with a velocity of 10 km per hour and has gone a distance of 30 km.
+                </p>
+                <p>
+                  Can you figure out how long ago the race started — the “age” of the race? (Assume each runner has maintained a consistent speed for the entire race).
+                </p>
+              </v-col>
+              <v-col
+                cols="4"
+              >
+                <v-alert
+                  class="text-center"
+                  min-height="200px"
+                >
+                  place picture here
+                </v-alert>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col
+                col="12"
+              >
+                <p>The race began:</p>
+                <mc-radiogroup
+                  :radio-options="[
+                    '1/3 of an hour ago.',
+                    '3 hours ago.',
+                    '12 hours ago.',
+                    '75 hours ago.'
+                  ]"
+                  :feedbacks="[
+                    'Recorded. You think the race began 1/3 of an hour ago.',
+                    'Recorded. You think the race began 3 hours ago.',
+                    'Recorded. You think the race began 12 hours ago.',
+                    'Recorded. You think the race began 75 hours ago.'
+                  ]"
+                  :correct-answers="[1]"
+                  :neutral-answers='[0,2,3]'
+                  :selected-callback="(state) => { $emit('ready'); }"
+                >
+                </mc-radiogroup>
+              </v-col>
+            </v-row>
           </v-container>
         </div>
       </v-card-text>
